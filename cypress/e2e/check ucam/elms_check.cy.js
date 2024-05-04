c5196d6aa92633f3773cd9c0d3187bf824f4ef79
limon27121 <moontaser009@gmail.com>
2024-05-04 20:23:03 +0600

    update the home section test

--- cypress/e2e/check ucam/elms_check.cy.js
+++ cypress/e2e/check ucam/elms_check.cy.js
@@ -1,25 +1,17 @@
 describe('Check log in functionality', () => {
-    it('should accept input of a specific type', () => {
+
+     beforeEach("login credential",()=>{
       var expected_link="https://ucam.uiu.ac.bd/Security/LogIn.aspx"
-      // Visit the page with the input box
       cy.visit(expected_link);
 
-  
-      // Type a value into the input box
       cy.get('#logMain_UserName').type("011171060")
-
-      //check the value is available or not 
-      cy.get('#logMain_UserName').should("have.value","011171060")
-         
       cy.get('#logMain_Password').type("2353")
-      cy.get('#logMain_Password').should("have.value","2353")
-
-      //click the button
       cy.get('#logMain_Button1').click()
-
-      // Adjust the time as needed
-      cy.wait(3000) 
+      cy.wait(2000)
+     })
+    
       it("test the header part after log in ",()=>{
+        var expected_link="https://ucam.uiu.ac.bd/Security/Login.aspx"
         //check the image is visible or not
         cy.get('[alt="Image"]').should('be.visible')
         cy.get('[alt="Image"]').should('have.attr', 'src', '/Images/BannerLogo.gif')
@@ -28,8 +20,13 @@ describe('Check log in functionality', () => {
         cy.get('.semesterStatusLabel').contains("Current→")
         cy.get('.semesterStatus').contains("241 - Spring 2024 (Semester), 241 - Spring 2024 (Trimester)")
         cy.get('.loginStyle').contains("011171060")
+        cy.get("#ctl00_lblSeparate").contains("/")
         cy.get('.logoutStyle').contains("Logout")
-        cy.get('.logoutStyle').click().url().should(expected_link);
+        
+        //click the logout button from the home screen
+        cy.get('.logoutStyle').click()
+        //check that click the logout button the url is matched with expected link
+        cy.url().should('eq', expected_link);
         
         })
 
@@ -47,21 +44,27 @@ describe('Check log in functionality', () => {
 
           //check the navbar element and length with collapse menu
       it("test the navbar-collapse",()=>{
-        cy.get('#ctl00_menuMain ul.level1.nav.navbar-nav.static li.static').should('have.length', 5)
-        cy.get('#ctl00_menuMain ul.level1.nav.navbar-nav.static li.static a.level1.static').contains("Student Accounts")
-        cy.get('#ctl00_menuMain ul.level1.nav.navbar-nav.static li.static a.level1.static').contains("Transport Registration")
-        cy.get('#ctl00_menuMain ul.level1.nav.navbar-nav.static li.static a.level1.static').contains("COE")
-        cy.get('#ctl00_menuMain ul.level1.nav.navbar-nav.static li.static a.level1.static').contains("Admin Control")
-        cy.get('#ctl00_menuMain ul.level1.nav.navbar-nav.static li.static a.level1.static').contains("Registration")
+        // cy.get('#ctl00_menuMain ul.level1.nav.navbar-nav.static li.static').should('have.length', 5)
+
+        //this is the updated part code 
+        cy.get('#ctl00_menuMain>ul>li').should('have.length', 5)
+
+
+        cy.get('#ctl00_menuMain>ul>li>a').contains("Student Accounts")
+        cy.get('#ctl00_menuMain>ul>li>a').contains("Transport Registration")
+        cy.get('#ctl00_menuMain>ul>li>a').contains("COE")
+        cy.get('#ctl00_menuMain>ul>li>a').contains("Admin Control")
+        cy.get('#ctl00_menuMain>ul>li>a').contains("Registration")
       })
       
     })
    
-  })
+  
+
   
 
 
-
+// url test and title check the site
   describe('URL Test', () => {
     it('should check if the URL is correct', () => {
       const expectedURL = 'https://ucam.uiu.ac.bd/Security/LogIn.aspx'
@@ -158,7 +161,7 @@ describe('Check log in functionality', () => {
 
 
   describe('Footer Test', () => {
-    it.only('should contain the correct footer text', () => {
+    it('should contain the correct footer text', () => {
       cy.visit("https://ucam.uiu.ac.bd/Security/LogIn.aspx")
       cy.get('#logMain_UserName').type("011171060")
       cy.get('#logMain_Password').type("2353")
