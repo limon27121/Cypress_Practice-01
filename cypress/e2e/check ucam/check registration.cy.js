b81a4eac61e60e4e83888b74354c72ddc4b85f37
limon27121 <moontaser009@gmail.com>
2024-05-05 11:53:35 +0600

    test case added in ucam registration to my view section which is sub menu of registration navbar

--- cypress/e2e/check ucam/check registration.cy.js
+++ cypress/e2e/check ucam/check registration.cy.js
@@ -1,114 +1,6 @@
-// describe('check_registration', () => {
-//     it('check registration header', () => {
-//         var expected_link="https://ucam.uiu.ac.bd/Security/LogIn.aspx"
-//         cy.visit(expected_link);
+//log in home page then click the registration then click the toggle button then click the my view
 
-//       cy.get('#logMain_UserName').type("011171060")
-//       cy.get('#logMain_Password').type("2353")
-//       cy.get('#logMain_Button1').click()
-//       cy.wait(3000) 
-
-//       cy.get(':nth-child(5) > .level1').click()
-//       cy.wait(3000)
-
-//       cy.get('.staticHome a#ctl00_A1').contains("Home")
-//       cy.get('.staticHome a#ctl00_lbtnIQAC').contains("IQAC")
-//       cy.get(".navbar-toggle").should("exist");
-      
-//        // Check if Course Evaluation is present
-//       cy.get('ul.level1.nav.navbar-nav.static')
-//       .contains('Course Evaluation')
-//       .should('exist');
-
-//       //check dropdown list for course evaluation
-//       cy.get('ul.level2.dropdown-menu.dynamic')
-//       .contains('Evaluation Form')
-//       .should('exist');
-
-//     // Check if Registration is present
-//     cy.get('ul.level1.nav.navbar-nav.static')
-//       .contains('Registration')
-//       .should('exist');
-
-//         //check dropdown list for registration process
-
-//       cy.get('ul.level2.dropdown-menu.dynamic')
-//       .contains('Section Selection & Registration New')
-//       .should('exist');
-      
-//       cy.get('ul.level2.dropdown-menu.dynamic')
-//       .contains('Course Withdraw Request (New)')
-//       .should('exist');
-      
-//       cy.get('ul.level2.dropdown-menu.dynamic')
-//       .contains('Pre Advising (New Course)')
-//       .should('exist');
-
-//       cy.get('ul.level2.dropdown-menu.dynamic')
-//       .contains('Pre-Advising (Retake Course)')
-//       .should('exist');
-
-
-
-//     // Check if My Account is present
-//     cy.get('ul.level1.nav.navbar-nav.static')
-//       .contains('My Account')
-//       .should('exist');
-
-//         //check dropdown list for My_account process
-//       cy.get('ul.level2.dropdown-menu.dynamic')
-//       .contains('Change Password')
-//       .should('exist');
-
-//     // Check if My View is present
-//     cy.get('ul.level1.nav.navbar-nav.static')
-//       .contains('My View')
-//       .should('exist');
-
-//     //check dropdown list for My_view process
-
-//       cy.get('ul.level2.dropdown-menu.dynamic')
-//       .contains('Result History')
-//       .should('exist');
-
-//       cy.get('ul.level2.dropdown-menu.dynamic')
-//       .contains('My Class Routine')
-//       .should('exist');
-
-//       cy.get('ul.level2.dropdown-menu.dynamic')
-//       .contains('My Class Routine')
-//       .should('exist');
-//     })
-//   })
-
-
-
-//   describe('My First Test', () => {
-//     it.only('Does not do much!', () => {
-//         var expected_link="https://ucam.uiu.ac.bd/Security/LogIn.aspx"
-//         cy.visit(expected_link);
-    
-//         cy.get('#logMain_UserName').type("011171060")
-//         cy.get('#logMain_Password').type("2353")
-//         cy.get('#logMain_Button1').click()
-//         cy.wait(3000) 
-  
-//         cy.get(':nth-child(5) > .level1').click()
-//         cy.wait(3000)
-//     //  cy.get("container-fluid").should("exist")
-//     cy.get('li.dropdown')
-//     .contains('Course Evaluation')
-//     .click();
-
-//   // After clicking on the Course Evaluation, you may need to wait for the dropdown menu to appear
-//   // Then, you can click on the Evaluation Form link inside the dropdown menu
-//   cy.get('ul.level2.dropdown-menu.dynamic')
-//     .contains('Evaluation Form')
-//     .click();
-//     })
-//   })
-
-  describe('check functionality of registration process', () => {
+  describe('check functionality of My view => result history process', () => {
 
     beforeEach("login",()=>{
       var expected_link="https://ucam.uiu.ac.bd/Security/LogIn.aspx"
@@ -119,12 +11,72 @@
       cy.get('#logMain_Button1').click()
       cy.wait(3000) 
       cy.get(':nth-child(5) > .level1').contains('Registration').click()
+      // cy.get("#ctl00_menuMain")
+      // .contains("Registration")
+      // .click();
+
       cy.get('[aria-haspopup="ctl00_menuMain:submenu:14"] > .dropdown-toggle').click()
+
       cy.get(".level2.dropdown-menu.dynamic").contains("Result History").click();
     })
      
-       
+    it("test Student course history",()=>{
+      cy.get(".col-md-12 .col-lg-12").contains("Student Course History")
+      cy.get(".col-md-12 .col-lg-12").should("be.visible")
+      cy.get(".panel.panel-default.pp").should("be.visible");
+      cy.get(".panel.panel-default.pp").contains("Student ID :").should("be.visible");
+      //check input field value
+      cy.get("#ctl00_MainContainer_txtStudentId").should("have.value", "011171060");
+      cy.get("#ctl00_MainContainer_btnLoad").should("exist").click()
 
+
+      // Check if the student name label is available and contains the correct text
+    cy.get(".panel.panel-default.pp")
+      .contains("Student Name :")
+      .should("be.visible");
+
+    // Check if the student name value is correct
+    cy.get("#ctl00_MainContainer_lblStudentName")
+      .should("contain", "Md. Moontaseralam");
+
+
+      // Check if the program label is available and contains the correct text
+    cy.get(".panel.panel-default.pp")
+      .contains("Program :")
+      .should("be.visible");
+
+    // Check if the program value is correct
+    cy.get("#ctl00_MainContainer_lblStudentProgram")
+      .should("contain", "BSCSE");
+    })
+     
+    it("test another section of student course history",()=>{
+
+      cy.get(".panel.panel-default.pp").contains("CGPA :").should("be.visible")
+      cy.get("#ctl00_MainContainer_lblCGPA").contains("3.29")
+
+      cy.get(".panel.panel-default.pp").contains("Degree Req. :").should("be.visible")
+      cy.get("#ctl00_MainContainer_lblDegreeReq").contains("138.00")
+
+      cy.get(".panel.panel-default.pp").contains("Com. Credit(s) :").should("be.visible")
+      cy.get("#ctl00_MainContainer_lblCompletedCr").contains("138.00")
+
+      cy.get(".panel.panel-default.pp").contains("Com. Credit(s) :").should("be.visible")
+      cy.get("#ctl00_MainContainer_lblCompletedCr").contains("138.00")
+
+      cy.get(".panel.panel-default.pp").contains("Probation :").should("be.visible")
+      cy.get("#ctl00_MainContainer_lblProbation").contains("No probation!")
+
+      cy.get(".panel.panel-default.pp").contains("Active Status :").should("be.visible")
+      cy.get("#ctl00_MainContainer_lblActiveStatus").contains("Inactive")
+
+      cy.get(".panel.panel-default.pp").contains("Att. Credit(s) :").should("be.visible")
+      cy.get("#ctl00_MainContainer_lblAttemptedCr").contains("138.00")
+
+      cy.get(".panel.panel-default.pp").contains("Waived/Transferred Credit(s) :").should("be.visible")
+      cy.get("#ctl00_MainContainer_lblWaivedTransferCr").contains("0")
+
+    })
 
       it('trimester element test developed', () => {
        cy.get(".msgTitle").contains("Message: ")
@@ -150,11 +102,9 @@
 
     })
 
-
+   // test Trimester wise GPA and CGPA section
     it('Verifies that every row contains numeric values of the trimester-wise result table ', function () {
-      // Visit the webpage with the table
-      
-      // Use cy.get() to select the table element
+     
       cy.get('#ctl00_MainContainer_gvResult').within(() => {
           // Get all rows except the header row
           cy.get('tr.rowCss').each(($row, index) => {
@@ -171,8 +121,10 @@
             
 
 
-  
+  // test Result of completed/registered courses section
   it('Checks the presence and text of header elements', () => {
+
+    cy.get("#ctl00_MainContainer_lblRegistered").contains("Result of completed/registered courses")
   
     // Check if the header row exists
       cy.get('.tableHead').should('exist');
@@ -187,14 +139,27 @@
       cy.get('th').eq(6).should('contain', 'Course Status');
     });
   })
-    
-  
 
+ // test Transferred/Waived courses section
+  it("test Transferred/Waived courses",()=>{
+    cy.get("#ctl00_MainContainer_lblWaiver").contains("Transferred/Waived courses")
+
+    cy.get(".panel.panel-default.pp").should("be.visible")
+  })
+
+  //test fotter section
+  it('should contain the correct footer text', () => {      
+    // Assert that the footer element exists
+    cy.get('.footer').should('exist');
+    
+    // Assert that the footer text contains the expected content
+    cy.get('.footer')
+      .contains('Powered by Edusoft Consultants Ltd')
+      .contains('Copyright © 2013 - 2024 Edusoft Consultants Ltd. All rights reserved.');
+  });
 
 });
     
-   
-  // });
   
 
 
